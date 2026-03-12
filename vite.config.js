6c85fb1719e32b33545bcd0947fa313d166995cd
DevK-26 <khushiy9697@gmail.com>
2026-03-12 23:16:43 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   preview: {
-    allowedHosts: ['portfolio-site-kvdg.onrender.com'],
+    allowedHosts: ['khushi.codescriet.dev'],
   },
 })
